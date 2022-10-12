bd82e9c8175d9a29ba71d43173770fcff9b9df9a
Homagni Saha <homagnisaha@gmail.com>
2022-10-12 14:12:52 -0400

    Update start_container.bash

--- scripts/start_container.bash
+++ scripts/start_container.bash
@@ -5,6 +5,6 @@
 
 xhost +
 
-sudo nvidia-docker run --rm -ti --mount type=bind,source=$(pwd),target=/slam --privileged --net=host --ipc=host -e DISPLAY=$DISPLAY -v /tmp/.X11-unix:/tmp/.X11-unix --env="QT_X11_NO_MITSHM=1" kinfusion:latest
+sudo nvidia-docker run --rm -ti --mount type=bind,source=$(pwd),target=/slam --privileged --net=host --ipc=host -e DISPLAY=$DISPLAY -v /tmp/.X11-unix:/tmp/.X11-unix --env="QT_X11_NO_MITSHM=1" homagni/kinfusion:latest
 
 
